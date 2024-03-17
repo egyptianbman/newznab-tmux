ae6619d3e76b73bb943bb6035453609f21643e76
Beshoy Girgis <shoy@1ds.us>
2024-03-17 14:23:06 -0500

    Fixes to powerlines
    Signed-off-by: Beshoy Girgis <shoy@1ds.us>

--- misc/update/tmux/run.php
+++ misc/update/tmux/run.php
@@ -91,22 +91,22 @@ function start_apps($tmux_session): void
     }
 
     if ((int) $console_bash === 1) {
-        Process::run("tmux new-window -t $tmux_session -n bash 'printf \"\033]2;Bash\033\" && bash -i'");
+        Process::run("tmux new-window -t $tmux_session -n bash 'printf \"\033]2;Bash\033\" && zsh -i'");
     }
 }
 
 function window_utilities($tmux_session)
 {
     Process::run("tmux new-window -t $tmux_session -n utils 'printf \"\033]2;fixReleaseNames\033\"'");
-    Process::run("tmux splitw -t $tmux_session:1 -v -p 50 'printf \"\033]2;updateTVandTheaters\033\"'");
-    Process::run("tmux selectp -t $tmux_session:1.0; tmux splitw -t $tmux_session:1 -h -p 50 'printf \"\033]2;removeCrapReleases\033\"'");
-    Process::run("tmux selectp -t $tmux_session:1.2; tmux splitw -t $tmux_session:1 -h -p 50 'printf \"\033]2;decryptHashes\033\"'");
+    Process::run("tmux splitw -t $tmux_session:1 -v 'printf \"\033]2;updateTVandTheaters\033\"'");
+    Process::run("tmux selectp -t $tmux_session:1.0; tmux splitw -t $tmux_session:1 -h 'printf \"\033]2;removeCrapReleases\033\"'");
+    Process::run("tmux selectp -t $tmux_session:1.2; tmux splitw -t $tmux_session:1 -h 'printf \"\033]2;decryptHashes\033\"'");
 }
 
 function window_stripped_utilities($tmux_session)
 {
     Process::run("tmux new-window -t $tmux_session -n utils 'printf \"\033]2;updateTVandTheaters\033\"'");
-    Process::run("tmux selectp -t $tmux_session:1.0; tmux splitw -t $tmux_session:1 -h -p 50 'printf \"\033]2;postprocessing_amazon\033\"'");
+    Process::run("tmux selectp -t $tmux_session:1.0; tmux splitw -t $tmux_session:1 -h 'printf \"\033]2;postprocessing_amazon\033\"'");
 }
 
 function window_ircscraper($tmux_session)
@@ -117,8 +117,8 @@ function window_ircscraper($tmux_session)
 function window_post($tmux_session)
 {
     Process::run("tmux new-window -t $tmux_session -n post 'printf \"\033]2;postprocessing_additional\033\"'");
-    Process::run("tmux splitw -t $tmux_session:2 -v -p 67 'printf \"\033]2;postprocessing_non_amazon\033\"'");
-    Process::run("tmux splitw -t $tmux_session:2 -v -p 50 'printf \"\033]2;postprocessing_amazon\033\"'");
+    Process::run("tmux splitw -t $tmux_session:2 -v 'printf \"\033]2;postprocessing_non_amazon\033\"'");
+    Process::run("tmux splitw -t $tmux_session:2 -v 'printf \"\033]2;postprocessing_amazon\033\"'");
 }
 
 function attach($tmuxPath, $tmux_session): void
@@ -137,23 +137,23 @@ $tmuxConfig = $tmuxPath.'tmux.conf';
 
 if ((int) $seq === 1) {
     Process::run("cd {$tmuxPath}; tmux -f $tmuxConfig new-session -d -s $tmux_session -n Monitor 'printf \"\033]2;\"Monitor\"\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h -p 67 'printf \"\033]2;update_releases\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -v -p 25 'printf \"\033]2;nzb-import\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h 'printf \"\033]2;update_releases\033\"'");
+    Process::run("tmux selectp -t $tmux_session:1.0; tmux splitw -t $tmux_session:0 -v 'printf \"\033]2;nzb-import\033\"'");
 
     window_utilities($tmux_session);
     window_post($tmux_session);
 } elseif ((int) $seq === 2) {
     Process::run("cd {$tmuxPath}; tmux -f $tmuxConfig new-session -d -s $tmux_session -n Monitor 'printf \"\033]2;\"Monitor\"\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h -p 67 'printf \"\033]2;sequential\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -v -p 25 'printf \"\033]2;nzb-import\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h 'printf \"\033]2;sequential\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -v 'printf \"\033]2;nzb-import\033\"'");
 
     window_stripped_utilities($tmux_session);
 } else {
     Process::run("cd {$tmuxPath}; tmux -f $tmuxConfig new-session -d -s $tmux_session -n Monitor 'printf \"\033]2;Monitor\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h -p 67 'printf \"\033]2;update_binaries\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -v -p 25 'printf \"\033]2;nzb-import\033\"'");
-    Process::run("tmux selectp -t $tmux_session:0.2; tmux splitw -t $tmux_session:0 -v -p 67 'printf \"\033]2;backfill\033\"'");
-    Process::run("tmux splitw -t $tmux_session -v -p 50 'printf \"\033]2;update_releases\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -h 'printf \"\033]2;update_binaries\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.0; tmux splitw -t $tmux_session:0 -v 'printf \"\033]2;nzb-import\033\"'");
+    Process::run("tmux selectp -t $tmux_session:0.2; tmux splitw -t $tmux_session:0 -v 'printf \"\033]2;backfill\033\"'");
+    Process::run("tmux splitw -t $tmux_session -v 'printf \"\033]2;update_releases\033\"'");
 
     window_utilities($tmux_session);
     window_post($tmux_session);
